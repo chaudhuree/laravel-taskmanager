f156fb18b3894d7f7bd6305dfde40d5c9a86a352
chaudhuree <chaudhuree00@gmail.com>
2024-03-15 06:41:58 +0600

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Laravel 10 Task List App</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+  @yield('styles')
+</head>
+
+<body>
+  <h1>@yield('title')</h1>
+  <div>
+  @if (session()->has('success'))
+      <div>{{ session('success') }}</div>
+    @endif
+
+    @yield('content')
+  </div>
+</body>
+
+</html>
